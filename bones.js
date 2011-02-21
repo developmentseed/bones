7c7e638bfffffe48051e80f4bd67c50ffc1b0bfb
Young Hahn <young@developmentseed.org>
2011-02-21 01:56:23 -0500

    first commit

---
+++ bones.js
@@ -0,0 +1,140 @@
+// Backbone base class overrides for the server-side context. DOM-related
+// modules are required *globally* such that Backbone will pick up their
+// presence.
+$ = require('jquery-1.4.4');
+jQuery = require('jquery-1.4.4');
+jsdom  = require('jsdom').jsdom;
+window = jsdom().createWindow();
+document = window.document;
+
+var fs = require('fs'),
+    _ = require('underscore')._,
+    Backbone = require('backbone'),
+    Handlebars = require('handlebars'),
+    clientJS = clientJS || fs.readFileSync(__dirname + '/client.js', 'utf8');
+
+// Bones object.
+var Bones = module.exports = {
+    Bones: function(server, options) {
+        // Add Backbone routing.
+        server.use(Backbone.history.middleware());
+
+        // Add route rule for bones.js.
+        server.get('/bones.js', function(req, res, next) {
+            res.send(Bones.clientJS(),  { 'Content-Type': 'text/javascript' });
+        });
+
+        // Add server reference to Bones.
+        this.server = server;
+        return this;
+    },
+    server: null,
+    templates: {},
+    clientJS: function() {
+        return clientJS + [
+            '// Bones object (client-side)',
+            'var Bones = {',
+            '    templates: ' + JSON.stringify(this.templates),
+            '};'
+        ].join('\n');
+    }
+};
+
+// View (server-side)
+// ------------------
+// With a server-side DOM present Backbone Views tend to take care of
+// themselves. The main override is to clear out `delegateEvents()` - the
+// `events` hash is of no use on the server-side with the View being dead
+// after initial delivery. `template()` and `html()` serve as 
+// functions to make client/server-side templating a uniform interface.
+//
+// The following conventions must be followed in order to ensure that the views
+// can be used in both environments:
+//
+// - Use `render()` only for templating. Any DOM event handlers, other
+//   js library initialization (e.g. OpenLayers) should be done in the
+//   `attach()` method.
+// - `render()` must `return this` in order to be chainable and any calls to
+//   `render()` should chain `trigger('attach')`.
+// - `template()` should be used to render an object using `template` to
+//   identify the template to use in the `Bones.templates` hash. Avoid using
+//   jquery or other doing other DOM element creation if templating could get
+//   the job done.
+Backbone.View = Backbone.View.extend({
+    delegateEvents: function() {},
+    template: function(template, data) {
+        var compiled = Handlebars.compile(Bones.templates[template]);
+        return compiled(data);
+    },
+    html: function() {
+        if (typeof this.el === 'string') {
+            return this.el;
+        } else {
+            // Consider using the method described here:
+            // http://stackoverflow.com/questions/652763/jquery-object-to-string
+            return $(this.el).html();
+        }
+    }
+});
+
+// Controller/History (server-side)
+// --------------------------------
+// Expose Backbone's controller/history routing functionality to Connect
+// as a middleware. A Connect server can add Backbone controller routing to
+// its stack of middlewares by doing:
+//
+//     server.use(Backbone.history.middleware());
+//     new MyController(); /* Routes will be added at initialize. */
+Backbone.Controller = Backbone.Controller.extend({
+    // Override `.route()` to add a callback handler with an additional
+    // `response` argument callback for sending back a View as HTML using
+    // the Connect server.
+    route: function(route, name, callback) {
+        Backbone.history || (Backbone.history = new Backbone.History);
+        if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+        Backbone.history.route(route, _.bind(function(fragment, res) {
+            var response = function(view) {
+                res.send(view.html());
+            };
+
+            var args = this._extractParameters(route, fragment);
+            var view = callback.apply(this, args.concat([response]));
+            this.trigger.apply(this, ['route:' + name].concat(args));
+        }, this));
+    }
+});
+
+// Override `.loadUrl()` to allow `res` response object to be passed through
+// to handler callback.
+Backbone.History.prototype.loadUrl = function(fragment, res) {
+    var matched = _.any(this.handlers, function(handler) {
+        if (handler.route.test(fragment)) {
+            handler.callback(fragment, res);
+            return true;
+        }
+    });
+    return matched;
+};
+
+// Provide a custom Backbone.History middleware for use with Connect.
+Backbone.History.prototype.middleware = function() {
+    var that = this;
+    return function(req, res, next) {
+        var fragment;
+        if (req.query && req.query['_escaped_fragment_']) {
+            fragment = req.query['_escaped_fragment_'];
+        } else {
+            fragment = req.url;
+        }
+        !that.loadUrl(fragment, res) && next();
+    };
+};
+
+// Clear out unused/unusable methods.
+Backbone.History.prototype.start = function() {};
+Backbone.History.prototype.getFragment = function() {};
+Backbone.History.prototype.saveLocation = function() {};
+
+// Instantiate Backbone.history.
+Backbone.history || (Backbone.history = new Backbone.History);
+
---
+++ client.js
@@ -0,0 +1,35 @@
+// Client-side `Backbone.View` overrides. Adds an `attach()` method that can be
+// triggered after `render()` to allow client-side specific JS event handlers,
+// UI libraries to be attached or inited. `template()` and `html()` are mirrors
+// of their server-side counterparts for templating and easy generation of a
+// View's HTML contents.
+Backbone.View = Backbone.View.extend({
+    attach: function() {},
+    _configure_original: Backbone.View.prototype._configure,
+    _configure: function(options) {
+        this._configure_original(options);
+        this.bind('attach', this.attach);
+    },
+    template: function(template, data) {
+        var compiled = Handlebars.compile(Bones.templates[template]);
+        return compiled(data);
+    },
+    html: function() {
+        return $(this.el).html();
+    }
+});
+
+// Client-side `Backbone.Controller` overrides.
+Backbone.Controller = Backbone.Controller.extend({
+    route: function(route, name, callback) {
+        Backbone.history || (Backbone.history = new Backbone.History);
+        if (!_.isRegExp(route)) route = this._routeToRegExp(route);
+        Backbone.history.route(route, _.bind(function(fragment, res) {
+            var args = this._extractParameters(route, fragment);
+            var response = function() {};
+            var view = callback.apply(this, args.concat([response]));
+            this.trigger.apply(this, ['route:' + name].concat(args));
+        }, this));
+    }
+});
+
