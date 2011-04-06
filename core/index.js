b3c63eca71ff040af1a5df42d5443ec6b5133057
Konstantin Käfer <github@kkaefer.com>
2011-04-06 15:50:01 -0400

    send templates to the client

--- core/index.js
+++ core/index.js
@@ -1,1 +1,30 @@
+var jade = require('jade');
+var path = require('path');
+var fs = require('fs');
+
 module.exports = require('..').plugin(__dirname).load();
+
+// Remove common prefix between __dirname and filename so that we don't
+// leak information about the directory structure.
+function removePrefix(str) {
+    for (var i = 0; i < str.length; i++) {
+        if (str[i] !== __dirname[i]) {
+            return str.substring(i);
+        }
+    }
+    return '';
+};
+
+require.extensions['.jade'] = function(module, filename) {
+    var content = fs.readFileSync(filename, 'utf8');
+    var name = path.basename(filename).replace(/\..+$/, '');
+    module.exports = jade.compile(content, {
+        filename: removePrefix(filename)
+    });
+    module.exports.register = function(server) {
+        server.templateSources.push({
+            filename: filename,
+            content: 'template = ' + this
+        });
+    };
+};
