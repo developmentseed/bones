c6ce21591a9ac9a1976b6eb87c473e65b98ecab7
Konstantin Käfer <github@kkaefer.com>
2011-04-07 11:48:10 -0400

    move directories to root

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = require('./lib');
+var plexus = exports;
+
+plexus.express = require('express');
+
+plexus.Plexus = require('./server/core');
+plexus.Backbone = require('./server/backbone');
+plexus.Plexus.Controller = require('./server/controller');
+plexus.Plexus.Model = require('./server/model');
+plexus.Plexus.Router = require('./server/router');
+plexus.Plexus.View = require('./server/view');
+plexus.Plexus.Server = require('./server/server');
+
+plexus.plugin = require('./server/plugin');
+plexus.middleware = require('./server/middleware');
