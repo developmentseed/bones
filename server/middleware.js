20b41d1e3a20a1340fd66a23879b65ffcb37a19d
Alex Barth <alex@developmentseed.org>
2011-04-19 17:24:55 -0400

    Not all errors expose a stack trace, simplify.

--- server/middleware.js
+++ server/middleware.js
@@ -15,26 +15,20 @@ middleware.csrf = function(conf) {
 
 middleware.txmtErrorHandler = function txmtErrorHandler(err, req, res, next) {
     res.statusCode = 500;
-    console.error(err.stack);
+    console.error(err);
+    var json = JSON.stringify(err);
     var accept = req.headers.accept || '';
     // html
     if (~accept.indexOf('html')) {
-        var stack = err.stack.replace(/(\/.+?):(\d+):(\d+)/g,
-            function(text, file, line, column) {
-                return '<a href="txmt://open?url=file://' + file +
-                        '&line=' + line + '&column=' + column +
-                        '">' + text + '</a>';
-            });
         res.setHeader('Content-Type', 'text/html');
-        res.end('<pre>' + stack + '</pre>');
+        res.end('<pre>' + json + '</pre>');
     // json
     } else if (~accept.indexOf('json')) {
-        var json = JSON.stringify({ error: err });
         res.setHeader('Content-Type', 'application/json');
         res.end(json);
     // plain text
     } else {
         res.writeHead(500, { 'Content-Type': 'text/plain' });
-        res.end(err.stack);
+        res.end(json);
     }
 };
