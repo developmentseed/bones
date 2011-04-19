71357df3e803b5532edfd1ac9564cdafe2942c47
Jeff Miccolis <jeff@miccolis.net>
2011-04-19 15:33:01 -0400

    Adding collection support.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ bones.Bones = require('./server/core');
 bones.Backbone = require('./server/backbone');
 bones.Bones.Controller = require('./server/controller');
 bones.Bones.Model = require('./server/model');
+bones.Bones.Collection = require('./server/collection');
 bones.Bones.Router = require('./server/router');
 bones.Bones.View = require('./server/view');
 bones.Bones.Server = require('./server/server');
