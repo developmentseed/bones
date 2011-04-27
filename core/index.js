ccb7394c8645ba4872a66cb7848f73e7ac1c11af
Konstantin Käfer <github@kkaefer.com>
2011-04-27 11:19:54 -0400

    new templating

--- core/index.js
+++ core/index.js
@@ -1,31 +1,20 @@
-var jade = require('jade');
+var _ = require('underscore');
 var path = require('path');
 var fs = require('fs');
+var bones = require('..');
 
-module.exports = require('..').plugin(__dirname);
+module.exports = bones.plugin(__dirname);
 module.exports.load();
 
-// Remove common prefix between __dirname and filename so that we don't
-// leak information about the directory structure.
-function removePrefix(str) {
-    for (var i = 0; i < str.length; i++) {
-        if (str[i] !== __dirname[i]) {
-            return str.substring(i);
-        }
-    }
-    return '';
-};
-
-require.extensions['.jade'] = function(module, filename) {
+require.extensions['._'] = function(module, filename) {
     var content = fs.readFileSync(filename, 'utf8');
-    var name = path.basename(filename).replace(/\..+$/, '');
-    module.exports = jade.compile(content, {
-        filename: removePrefix(filename)
-    });
+    var name = bones.Bones.camelize(path.basename(filename).replace(/\..+$/, ''));
+
+    module.exports = _.template(content);
     module.exports.register = function(server) {
         server.assets.templates.push({
             filename: filename,
-            content: 'template = ' + this
+            content: 'template = _.template(' + JSON.stringify(content) + ');'
         });
     };
 };
