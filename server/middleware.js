60beba2d146c0c8292dc1ca9082d2cf506ca17c6
Konstantin Käfer <github@kkaefer.com>
2011-05-10 10:48:33 -0400

    add a middleware() function instead of the array

---
+++ server/middleware.js
@@ -0,0 +1,28 @@
+exports = module.exports = require('express');
+
+exports['csrf'] = function() {
+    return function(req, res, next) {
+        if (req.method === 'GET') {
+            next();
+        } else if (req.body && req.cookies['bones.token'] && req.body['bones.token'] === req.cookies['bones.token']) {
+            delete req.body['bones.token'];
+            next();
+        } else {
+            res.send(403);
+        }
+    }
+};
+
+exports['fragmentRedirect'] = function() {
+    return function(req, res, next) {
+        // @see https://code.google.com/web/ajaxcrawling/docs/specification.html
+        if (req.query._escaped_fragment_ === undefined) {
+            next();
+        } else {
+            // Force the first char of the path to be a slash to prevent
+            // foreign redirects.
+            var path = '/' + req.query._escaped_fragment_.substr(1);
+            res.redirect(path, 301);
+        }
+    }
+};
