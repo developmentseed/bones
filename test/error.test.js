bf2a19226f6da3250b62c4b6f2189558cb1579c3
Konstantin Käfer <github@kkaefer.com>
2011-05-17 16:10:20 -0400

    test error messages

---
+++ test/error.test.js
@@ -0,0 +1,149 @@
+process.env.NODE_ENV = 'test';
+var assert = require('assert');
+
+require('./fixtures/demo');
+var demo = require('bones').plugin;
+var main = new demo.servers['Main'](demo);
+
+exports['error 404'] = function(beforeExit) {
+    assert.response(main.server, {
+        url: '/does-not-exist',
+        method: 'GET'
+    }, {
+        body: 'Not Found',
+        status: 404
+    });
+
+    assert.response(main.server, {
+        url: '/does-not-exist',
+        method: 'GET',
+        headers: { 'accept': 'application/json' }
+    }, {
+        body: '{"message":"Not Found"}',
+        status: 404
+    });
+
+    assert.response(main.server, {
+        url: '/does-not-exist',
+        method: 'POST',
+        headers: { 'accept': 'application/json' }
+    }, {
+        body: '{"message":"Forbidden"}',
+        status: 403
+    });
+
+    assert.response(main.server, {
+        url: '/does-not-exist',
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json',
+            'cookie': 'bones.token=cfd72122969dfaefddd180725fadf53f;'
+        },
+        body: JSON.stringify({ 'bones.token': 'cfd72122969dfaefddd180725fadf53f' })
+    }, {
+        body: 'Not Found',
+        status: 404
+    });
+
+    assert.response(main.server, {
+        url: '/does-not-exist',
+        method: 'POST',
+        headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'cookie': 'bones.token=cfd72122969dfaefddd180725fadf53f;'
+        },
+        body: JSON.stringify({ 'bones.token': 'cfd72122969dfaefddd180725fadf53f' })
+    }, {
+        body: '{"message":"Not Found"}',
+        status: 404
+    });
+
+    assert.response(main.server, {
+        url: '/api/DoesNotExit/asdf',
+        method: 'GET'
+    }, {
+        body: 'Not Found',
+        status: 404
+    });
+
+    assert.response(main.server, {
+        url: '/api/DoesNotExit/asdf',
+        method: 'GET',
+        headers: { 'accept': 'application/json' }
+    }, {
+        body: '{"message":"Not Found"}',
+        status: 404
+    });
+
+    assert.response(main.server, {
+        url: '/api/Page/asdf',
+        method: 'GET'
+    }, {
+        body: 'Not Found',
+        status: 404
+    });
+
+    assert.response(main.server, {
+        url: '/api/Page/asdf',
+        method: 'GET',
+        headers: { 'accept': 'application/json' }
+    }, {
+        body: '{"message":"Not Found"}',
+        status: 404
+    });
+
+    assert.response(main.server, {
+        url: '/api/Page/asdf',
+        method: 'PUT',
+        headers: {
+            'content-type': 'application/json',
+            'cookie': 'bones.token=cfd72122969dfaefddd180725fadf53f;'
+        },
+        body: JSON.stringify({ 'bones.token': 'cfd72122969dfaefddd180725fadf53f' })
+    }, {
+        body: 'Conflict',
+        status: 409
+    });
+
+    assert.response(main.server, {
+        url: '/api/Page/asdf',
+        method: 'PUT',
+        headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'cookie': 'bones.token=cfd72122969dfaefddd180725fadf53f;'
+        },
+        body: JSON.stringify({ 'bones.token': 'cfd72122969dfaefddd180725fadf53f' })
+    }, {
+        body: '{"message":"Conflict"}',
+        status: 409
+    });
+
+    assert.response(main.server, {
+        url: '/api/Page/asdf',
+        method: 'DELETE',
+        headers: {
+            'content-type': 'application/json',
+            'cookie': 'bones.token=cfd72122969dfaefddd180725fadf53f;'
+        },
+        body: JSON.stringify({ 'bones.token': 'cfd72122969dfaefddd180725fadf53f' })
+    }, {
+        body: 'Conflict',
+        status: 409
+    });
+
+    assert.response(main.server, {
+        url: '/api/Page/asdf',
+        method: 'DELETE',
+        headers: {
+            'accept': 'application/json',
+            'content-type': 'application/json',
+            'cookie': 'bones.token=cfd72122969dfaefddd180725fadf53f;'
+        },
+        body: JSON.stringify({ 'bones.token': 'cfd72122969dfaefddd180725fadf53f' })
+    }, {
+        body: '{"message":"Conflict"}',
+        status: 409
+    });
+};
