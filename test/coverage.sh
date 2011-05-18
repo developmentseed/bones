e2384db2196a57e2705be7fb0d811ee7122ad2e4
Konstantin Käfer <github@kkaefer.com>
2011-05-17 23:09:37 -0400

    exclude prefixes and suffixes

--- test/coverage.sh
+++ test/coverage.sh
@@ -7,6 +7,18 @@ jscoverage --no-instrument=test \
            --no-instrument=node_modules \
            --no-instrument=assets \
            --no-instrument=client \
+           --no-instrument=server/command.prefix.js \
+           --no-instrument=server/command.suffix.js \
+           --no-instrument=server/controller.prefix.js \
+           --no-instrument=server/controller.suffix.js \
+           --no-instrument=server/model.prefix.js \
+           --no-instrument=server/model.suffix.js \
+           --no-instrument=server/router.prefix.js \
+           --no-instrument=server/router.suffix.js \
+           --no-instrument=server/server.prefix.js \
+           --no-instrument=server/server.suffix.js \
+           --no-instrument=server/view.prefix.js \
+           --no-instrument=server/view.suffix.js \
            --exclude=examples \
            bones \
            coverage/node_modules/bones
