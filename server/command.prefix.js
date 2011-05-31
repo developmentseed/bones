771796d39b2e1da15a8551b2042e5ebde90cd0bd
Young Hahn <young@developmentseed.org>
2011-05-31 00:14:17 -0400

    Remove more router references.

--- server/command.prefix.js
+++ server/command.prefix.js
@@ -8,7 +8,6 @@ var models = Bones.plugin.models;
 var views = Bones.plugin.views;
 var controllers = Bones.plugin.controllers;
 var templates = Bones.plugin.templates;
-var routers = Bones.plugin.routers;
 var servers = Bones.plugin.servers;
 var commands = Bones.plugin.commands;
 
--- server/server.prefix.js
+++ server/server.prefix.js
@@ -10,7 +10,6 @@ var models = Bones.plugin.models;
 var views = Bones.plugin.views;
 var controllers = Bones.plugin.controllers;
 var templates = Bones.plugin.templates;
-var routers = Bones.plugin.routers;
 var servers = Bones.plugin.servers;
 
 var server;
