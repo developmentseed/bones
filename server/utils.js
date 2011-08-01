9f741adf7007dfcddc12ae6c4530840ca44e95ae
Konstantin Käfer <github@kkaefer.com>
2011-08-01 15:00:45 -0400

    pass the file descriptor to isatty

--- server/utils.js
+++ server/utils.js
@@ -24,7 +24,7 @@ var styles = {
     underline: 4
 };
 
-if (tty.isatty()) {
+if (tty.isatty(process.stdout.fd)) {
     utils.colorize = function(text, color, style) {
         color = color || 'red';
         style = style || 'regular';
