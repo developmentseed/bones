0f8269236553c431ad68f22d2f82939911bf5fea
Young Hahn <young@developmentseed.org>
2011-08-22 12:41:19 -0400

    Fix for failure to serialize jQuery event object.

--- assets/debug.js
+++ assets/debug.js
@@ -1,4 +1,7 @@
 window.onerror = function(message, url, line) {
+    // Don't attempt to handle non-standard errors (e.g. failed
+    // HTTP request via jQuery).
+    if (typeof message !== 'string') return;
     $.ajax('/api/Error', {
         data: {
             message: message,
