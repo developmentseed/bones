a62a4606759b6383f78ddc6685a2b70e4400bb19
Makara Wang <makara15@gmail.com>
2012-05-21 23:50:49 +0800

    Revert "Remove Backbone.Events.on() from server side. It conflicts with Node.js events"
    This reverts commit d2b7142128f14d110d99f91c712bf98f8544f799.

--- server/backbone.js
+++ server/backbone.js
@@ -3,13 +3,3 @@ var Backbone = module.exports = require('../shared/backbone');
 Backbone.sync = function() {
     throw new Error('No default sync method');
 };
-
-// Remove Backbone.Events.on() from server side. It conflicts with Node.js
-// events. Remove off() together as it can be confusing. Please use bind() and
-// unbind().
-if (_(Backbone.Events).has('on')) {
-    delete Backbone.Events.on;
-}
-if (_(Backbone.Events).has('off')) {
-    delete Backbone.Events.off;
-}
